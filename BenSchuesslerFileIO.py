9169bf923db744313b0eaf0e9d08c2a81b9352b1
benschuessler <135157387+benschuessler@users.noreply.github.com>
2023-12-13 09:26:01 -0500

    ghdhgfh

--- BenSchuesslerFileIO.py
+++ BenSchuesslerFileIO.py
@@ -11,15 +11,18 @@ TestCases:
 5) Make selections from the database using user input values (check_zipcode) (check_info)
 6) include no match results (check info)
 7) add new data records to data base (check_add_data)
-8) List students by first name, last name sorted by last name (check_names)
-9) Write any given output to a file as ".csv" (check_add_data) (check_remove_data)
 10) Program Repeats
-11) Use functions to organize the program
-12) Create menu to execute functions
-13) Delete records from database (check_remove_data)
+9) List students by first name, last name sorted by last name (check_names)
+9a) Write any given output to a file as ".csv" (check_add_data) (check_remove_data)
+9b) Program Repeats
+10) Use functions to organize the program
+10a) Create menu to execute functions
+12) Delete records from database (check_remove_data)
 14) Create graphs with data (graph_students)
 """ 
-
+from pathlib import Path
+current_dir = Path(__file__).parent
+file_path = current_dir / "FileIOData.csv"
 
 import csv
 import matplotlib.pyplot as plt #program providing graph (boys vs. girls graph)
@@ -27,7 +30,7 @@ def main():
    
    
     """ doc """
-    file_input = open(r"c:\Users\bschuessler26\Downloads/FileIOData.csv")
+    file_input = open(file_path)
 
     file_input.readline()                       #skip first line of header info
     answer = "Y"
@@ -89,6 +92,8 @@ def check_seniors(file_in):
 
     for record in file_in:
         kid = record.split(",")
+        if kid[0] == '\n':
+            continue
         if kid[3] == "12":
             seniors += 1
     print(seniors)
@@ -104,6 +109,8 @@ def check_males(file_in):
 
     for record in file_in:
         kid = record.split(",")
+        if kid[0] == '\n':
+            continue
         if kid[4] == "M":
             males += 1
     print(males)
@@ -117,6 +124,8 @@ def check_females(file_in):
     
     for record in file_in:
         kid = record.split(",")
+        if kid[0] == '\n':
+            continue
         if kid[4] == "F":
             females += 1
     print(females)
@@ -130,6 +139,8 @@ def check_ct(file_in):
 
     for record in file_in:
         kid = record.split(",")
+        if kid[0] == '\n':
+            continue
         if kid[8] == "CT":
             ct += 1
     print(ct)
@@ -143,6 +154,8 @@ def check_ny(file_in):
 
     for record in file_in:
         kid = record.split(",")
+        if kid[0] == '\n':
+            continue
         if kid[8] == "NY":
             ny += 1
     print(ny)
@@ -153,14 +166,17 @@ def check_info(file_in):
     #Takes students first and last name: kid[0], kid[2]
     #returns all of students information: kid
     student = input("enter your students first and last name").split(' ')
-    
+    output = False
     file_in.seek(1)
     for record in file_in:
         kid = record.split(",")
+        if kid[0] == '\n':
+            continue
 
         if student[0] == kid [0] and student[1] == kid [2]:
             print(kid)
-    else:
+            output = True
+    if output == False:
          print ("no match")
 
 def check_zipcode(file_in):
@@ -171,9 +187,16 @@ def check_zipcode(file_in):
 
     zip_counts = {}
 
-    file_in.seek(1)
+    file_in.seek(2)
+    a = 1
     for record in file_in:
         kid = record.split(",")
+        if kid[0] == '\n':
+            continue
+        if a or kid[0] in ['Oya']:
+            a = False
+            continue
+        kid[9] = kid[9][:-1]
 
         if kid[9] in zip_counts.keys():
             zip_counts[kid[9]] += 1
@@ -189,10 +212,16 @@ def check_ziptally(file_in):
 
     zip_counts = {}
 
-    file_in.seek(1)
+    file_in.seek(2)
+    a = 1
     for record in file_in:
         kid = record.split(",")
-
+        if kid[0] == '\n':
+            continue
+        if a or kid[0] in ['Oya']:
+            a = False
+            continue
+        kid[9] = kid[9][:-1]
         if kid[9] in zip_counts.keys():
             zip_counts[kid[9]] += 1
         else:
@@ -209,6 +238,8 @@ def graph_students(file_in):
 
     for record in file_in:
         kid = record.split(",")
+        if kid[0] == '\n':
+            continue
         total += 1
         if kid[4].strip() == "M":
             boys += 1
@@ -230,6 +261,8 @@ def check_names(file_in):
     file_in.seek(1)
     for record in file_in:
         kid = record.split(",")
+        if kid[0] == '\n':
+            continue
         print(kid[0], kid[2])
 
 def check_add_data(file_in):
@@ -238,7 +271,7 @@ def check_add_data(file_in):
     #Returns: void
     add_info = input("Type in the data for a student. Input their first name, middle name, last name, grade, gender (M/F), teacher's first and last name, town, state, zipcode: (seperate all info by commas)")
     add = add_info.split(",")
-    f = open(r"c:\Users\bschuessler26\Downloads/FileIOData.csv", "a")
+    f = open(file_path,"a+")
     var_name = csv.writer(f)
     var_name.writerow(add)                                                   
     f.close()
@@ -252,12 +285,14 @@ def check_remove_data(file_in):
     new_list = []
     for record in file_in:
         kid = record.split(",")
+        if kid[0] == '\n':
+            continue
         try:
             a = kid[2]
         except:
             print(kid)
         if kid[2] == remove_lastname:
-            continue
+            print("deleted")    
         else:
             new_list.append(record)
 
